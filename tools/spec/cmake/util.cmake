7450366a0c62d2c58a88cb6cca052247f76e463c
ZSaberLv0 <z@zsaber.com>
2023-04-02 11:54:13 +0800

    update

--- tools/spec/cmake/util.cmake
+++ tools/spec/cmake/util.cmake
@@ -1,15 +1,34 @@
 
-function(strip_file_macro targetname)
+function(zfprojStripFILE targetName)
     if(WIN32)
     else()
-        target_compile_options(${targetname} PUBLIC "-Wno-builtin-macro-redefined")
+        target_compile_options(${targetName} PUBLIC "-Wno-builtin-macro-redefined")
     endif()
-    get_target_property(source_files "${targetname}" SOURCES)
+    get_target_property(source_files "${targetName}" SOURCES)
     foreach(sourcefile ${source_files})
         get_property(defs SOURCE "${sourcefile}" PROPERTY COMPILE_DEFINITIONS)
         get_filename_component(filename "${sourcefile}" NAME)
         list(APPEND defs "__FILE__=\"${filename}\"")
         set_property(SOURCE "${sourcefile}" PROPERTY COMPILE_DEFINITIONS ${defs})
     endforeach()
-endfunction()
+endfunction(zfprojStripFILE)
+
+function(zfprojLoadAllSymbol targetName)
+    if(WIN32)
+        set_target_properties(${targetName} PROPERTIES
+            LINK_FLAGS "/WHOLEARCHIVE"
+            )
+    elseif(APPLE)
+        set_target_properties(${targetName} PROPERTIES
+            LINK_FLAGS "-Wl,-all_load"
+            )
+    else()
+        set_target_properties(${targetName} PROPERTIES
+            LINK_FLAGS "-Wl,--whole-archive"
+            )
+        set_target_properties(${targetName} PROPERTIES
+            LINK_FLAGS "-Wl,--allow-multiple-definition"
+            )
+    endif()
+endfunction(zfprojLoadAllSymbol)
 
