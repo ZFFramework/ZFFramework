7a4b714c832f6a5ea51c6cee9bff5d1a5092ce93
ZSaberLv0 <z@zsaber.com>
2023-04-02 19:32:06 +0800

    update

--- ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
+++ ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
@@ -6,7 +6,27 @@ function(zfprojConfigBefore_ZF_impl projName ZF_SRC_FILES)
 endfunction(zfprojConfigBefore_ZF_impl)
 
 function(zfprojConfigAfter_ZF_impl projName)
+    add_subdirectory("${ZF_ROOT_PATH}/ZF/ZF_impl/zf3rd/_repo/SDL" SDL)
+    target_link_libraries(${projName} PUBLIC SDL2main SDL2-static)
+
     set_target_properties(${projName} PROPERTIES CXX_STANDARD 11)
     set_target_properties(${projName} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+
+    if(WIN32)
+        set_target_properties(${projName} PROPERTIES
+            LINK_FLAGS "/WHOLEARCHIVE"
+            )
+    elseif(APPLE)
+        set_target_properties(${projName} PROPERTIES
+            LINK_FLAGS "-Wl,-all_load"
+            )
+    else()
+        set_target_properties(${projName} PROPERTIES
+            LINK_FLAGS "-Wl,--whole-archive"
+            )
+        set_target_properties(${projName} PROPERTIES
+            LINK_FLAGS "-Wl,--allow-multiple-definition"
+            )
+    endif()
 endfunction(zfprojConfigAfter_ZF_impl)
 
