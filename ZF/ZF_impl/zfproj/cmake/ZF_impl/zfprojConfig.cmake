9d4dc4a3f087e8b9a374c2e88eb6334bd362beac
ZSaberLv0 <z@zsaber.com>
2023-04-02 14:00:49 +0800

    update

--- ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
+++ ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
@@ -1,4 +1,12 @@
 
+function(zfprojConfigGlobal_ZF_impl)
+endfunction(zfprojConfigGlobal_ZF_impl)
+
+function(zfprojConfigBefore_ZF_impl projName ZF_SRC_FILES)
+endfunction(zfprojConfigBefore_ZF_impl)
+
 function(zfprojConfigAfter_ZF_impl projName)
+    set_target_properties(${projName} PROPERTIES CXX_STANDARD 11)
+    set_target_properties(${projName} PROPERTIES POSITION_INDEPENDENT_CODE ON)
 endfunction(zfprojConfigAfter_ZF_impl)
 
