d746662023dd047e14828bc7be1f61c917d1d76c
ZSaberLv0 <z@zsaber.com>
2023-06-23 12:49:01 +0800

    update

--- ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
+++ ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
@@ -28,7 +28,7 @@ function(zfprojConfigAfter_ZF_impl projName)
     add_subdirectory("${ZF_ROOT_PATH}/ZF/ZF_impl/zf3rd/_repo/SDL_ttf" SDL_ttf)
     set_target_properties(SDL2_ttf PROPERTIES POSITION_INDEPENDENT_CODE ON)
 
-    target_link_libraries(${projName}
+    target_link_libraries(${projName} PUBLIC
         SDL2main
         SDL2-static
         SDL2_image
