207c9ef3a50b1d828fde8aacfd956ab293948b27
ZSaberLv0 <z@zsaber.com>
2024-11-25 18:10:09 +0800

    update

--- master/ZF_CI/upload_app.sh
+++ master/ZF_CI/upload_app.sh
@@ -23,29 +23,33 @@ REPO_PATH="$ZF_ROOT_PATH/_tmp/upload_app"
 REPO_PATH_TMP="$ZF_ROOT_PATH/_tmp/upload_app_tmp"
 
 sh "$ZF_ROOT_PATH/tools/common/git_check.sh" "https://github.com/ZFFramework/AppArchive" master "$REPO_PATH"
-
 _OLD_DIR=$(pwd)
 cd "$REPO_PATH"
 git config user.email "z@zsaber.com"
 git config user.name "ZSaberLv0"
 git config push.default "simple"
 git checkout .
-git reset --hard
+git reset --hard origin/master
 git clean -xdf
 git pull
+cd "$_OLD_DIR"
 
 sh "$ZF_ROOT_PATH/tools/common/copy_check.sh" "$REPO_PATH" "$REPO_PATH_TMP"
-cd "$_OLD_DIR"
-cp -r "$APP_PATH" "$REPO_PATH_TMP/$REMOTE_FILE"
+sh "$ZF_ROOT_PATH/tools/common/zfsh/rm.sh" "$REPO_PATH_TMP/$REMOTE_FILE"
+sh "$ZF_ROOT_PATH/tools/common/copy_check.sh" "$APP_PATH" "$REPO_PATH_TMP/$REMOTE_FILE"
+
 cd "$REPO_PATH"
 git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *' --prune-empty --tag-name-filter cat -- --all
+cd "$_OLD_DIR"
+
 sh "$ZF_ROOT_PATH/tools/common/copy_check.sh" "$REPO_PATH_TMP" "$REPO_PATH"
 
+cd "$REPO_PATH"
 git add -A
 git commit -a -m "update by CI"
 git push --force "https://ZSaberLv0:$ZFCI_TOKEN@github.com/ZFFramework/AppArchive"
 cd "$_OLD_DIR"
 
-rm -rf "$REPO_PATH"
-rm -rf "$REPO_PATH_TMP"
+# sh "$ZF_ROOT_PATH/tools/common/zfsh/rm.sh" "$REPO_PATH"
+sh "$ZF_ROOT_PATH/tools/common/zfsh/rm.sh" "$REPO_PATH_TMP"
 
