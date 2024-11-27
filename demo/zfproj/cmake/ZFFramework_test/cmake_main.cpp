b02a131085a9dbfd7e65daf5f17b0e0c5ca37f29
ZSaberLv0 <z@zsaber.com>
2024-11-28 00:06:30 +0800

    update

---
+++ demo/zfproj/cmake/ZFFramework_test/cmake_main.cpp
@@ -0,0 +1,35 @@
+#include "ZFCore.h"
+#include "ZFCore/protocol/ZFProtocolZFMainEntry.h"
+
+#include "ZFImpl/ZFImpl_env.h"
+
+#if ZF_ENV_sys_Windows
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    ZFFrameworkInit();
+
+    ZFCoreArray<zfstring> params;
+    ZFCoreArgSplit(params, lpCmdLine);
+    if(params.count() > 0) {
+        params.removeFirst();
+    }
+    zfint ret = ZFMainExecute(params);
+
+    ZFFrameworkCleanup();
+    return (int)ret;
+}
+#else // #if ZF_ENV_sys_Windows
+int main(int argc, char **argv) {
+    ZFFrameworkInit();
+
+    ZFCoreArray<zfstring> params;
+    for(int i = 1; i < argc; ++i) {
+        params.add(argv[i]);
+    }
+    zfint ret = ZFMainExecute(params);
+
+    ZFFrameworkCleanup();
+    return (int)ret;
+}
+#endif // #if ZF_ENV_sys_Windows
+
