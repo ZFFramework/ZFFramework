9df0556bfb342d2e510e725ef42e5cf376d5dc3d
ZSaberLv0 <z@zsaber.com>
2024-11-29 00:21:40 +0800

    update

--- ZF/ZFCore_impl/zfproj/cmake/ZFCore_impl/zfprojConfig.cmake
+++ ZF/ZFCore_impl/zfproj/cmake/ZFCore_impl/zfprojConfig.cmake
@@ -1,14 +1,10 @@
 
 function(zfprojConfigAfter_ZFCore_impl projName)
     target_compile_definitions(${projName} PUBLIC ZF_ENV_FORCE_sys_SDL=1)
-    if(NOT ZFSDL_PREFIX)
-        set(ZFSDL_PREFIX TRUE)
-        list(APPEND CMAKE_PREFIX_PATH "${ZF_ROOT_PATH}/_release/cmake/SDL")
-    endif()
 
     if(NOT ZFSDL_SDL2)
         set(ZFSDL_SDL2 TRUE)
-        find_package(SDL2 REQUIRED)
+        add_subdirectory(${ZF_ROOT_PATH}/ZF/ZF_impl/zf3rd/_repo/SDL SDL)
         target_link_libraries(${projName} SDL2::SDL2)
     endif()
 endfunction(zfprojConfigAfter_ZFCore_impl)
