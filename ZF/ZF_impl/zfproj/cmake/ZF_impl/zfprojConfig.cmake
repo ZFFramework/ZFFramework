581865cefb1e8e40ecdfd34bed038aa6ff203453
ZSaberLv0 <z@zsaber.com>
2024-12-03 12:57:51 +0800

    update

--- ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
+++ ZF/ZF_impl/zfproj/cmake/ZF_impl/zfprojConfig.cmake
@@ -38,33 +38,19 @@ endfunction(zfprojConfigBefore_ZF_impl)
 function(zfprojConfigAfter_ZF_impl projName)
     target_compile_definitions(${projName} PUBLIC ZF_ENV_FORCE_sys_SDL=1)
 
-    find_package(SDL2 CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
-    find_package(SDL2_ttf CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
-    find_package(SDL2_image CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
-
-    message("ZFDEBUG begin")
-    get_cmake_property(_vars VARIABLES)
-    list(SORT _vars)
-    foreach(_var ${_vars})
-        message("ZFDEBUG var: ${_var}=${${_var}}")
-    endforeach()
-    message("ZFDEBUG envs")
-    execute_process(COMMAND "${CMAKE_COMMAND}" "-E" "environment")
-    message("ZFDEBUG end")
-
     if(NOT TARGET SDL2::SDL2)
         find_package(SDL2 REQUIRED CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
         target_link_libraries(${projName} SDL2::SDL2)
     endif()
 
-    if(NOT TARGET SDL2_image::SDL2_image)
-        find_package(SDL2_image REQUIRED CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
-        target_link_libraries(${projName} SDL2_image::SDL2_image)
-    endif()
-
     if(NOT TARGET SDL2_ttf::SDL2_ttf)
         find_package(SDL2_ttf REQUIRED CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
         target_link_libraries(${projName} SDL2_ttf::SDL2_ttf)
+    endif()
+
+    if(NOT TARGET SDL2_image::SDL2_image)
+        find_package(SDL2_image REQUIRED CONFIG HINTS "${ZF_ROOT_PATH}/_release/cmake/SDL" NO_DEFAULT_PATH)
+        target_link_libraries(${projName} SDL2_image::SDL2_image)
     endif()
 endfunction(zfprojConfigAfter_ZF_impl)
 
