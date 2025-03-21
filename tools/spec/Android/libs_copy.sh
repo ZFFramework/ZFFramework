9d8b80e9e3c96ea052a5121a942940c44fa69042
ZSaberLv0 <z@zsaber.com>
2025-03-21 19:45:50 +0800

    update

--- tools/spec/Android/libs_copy.sh
+++ tools/spec/Android/libs_copy.sh
@@ -10,14 +10,14 @@ fi
 ZF_ROOT_PATH=$WORK_DIR/../../..
 
 mkdir -p "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/jniLibs" >/dev/null 2>&1
-sh "$ZF_ROOT_PATH/tools/common/copy_file_in_tree.sh" "lib${PROJ_NAME}.so" "$PROJ_PATH/zflib/build/intermediates/library_and_local_jars_jni/release/jni" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/jniLibs" >/dev/null 2>&1
+sh "$ZF_ROOT_PATH/tools/common/copy_file_in_tree.sh" "lib${PROJ_NAME}.so" "$PROJ_PATH/zflib/build/intermediates/library_and_local_jars_jni/release/copyReleaseJniLibsProjectAndLocalJars/jni" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/jniLibs" >/dev/null 2>&1
 sh "$ZF_ROOT_PATH/tools/util/copy_header.sh" "$PROJ_PATH/../../../zfsrc" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/cpp/include" >/dev/null 2>&1
 sh "$ZF_ROOT_PATH/tools/util/copy_res.sh" "$PROJ_PATH/../../../zfres" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/assets/zfres" >/dev/null 2>&1
 
-JAR_FILE_SIZE=`wc -c "$PROJ_PATH/zflib/build/intermediates/aar_main_jar/release/classes.jar" 2>/dev/null | awk '{print $1}' 2>/dev/null`
+JAR_FILE_SIZE=`wc -c "$PROJ_PATH/zflib/build/intermediates/aar_main_jar/release/syncReleaseLibJars/classes.jar" 2>/dev/null | awk '{print $1}' 2>/dev/null`
 if test ! "x-$JAR_FILE_SIZE" = "x-" && test $JAR_FILE_SIZE -ge 1024 ; then
     mkdir -p "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/libs" >/dev/null 2>&1
-    cp "$PROJ_PATH/zflib/build/intermediates/aar_main_jar/release/classes.jar" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/libs/$PROJ_NAME.jar" >/dev/null 2>&1
+    cp "$PROJ_PATH/zflib/build/intermediates/aar_main_jar/release/syncReleaseLibJars/classes.jar" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/src/main/libs/$PROJ_NAME.jar" >/dev/null 2>&1
 fi
 
 sh "$ZF_ROOT_PATH/tools/common/copy_check.sh" "$PROJ_PATH/zflib/build/outputs/aar/zflib-release.aar" "$ZF_ROOT_PATH/_release/Android/module/$PROJ_NAME/aar/$PROJ_NAME.aar" >/dev/null 2>&1
