313bccc39030aed0a680ab1b35057af503dff61e
ZSaberLv0 <z@zsaber.com>
2025-03-22 20:49:43 +0800

    update

--- demo/zfproj_with_src/Android/ZFFramework_test/zfapp/src/main/java/com/ZFFramework/Android/ZFFramework_test/Loader.java
+++ demo/zfproj_with_src/Android/ZFFramework_test/zfapp/src/main/java/com/ZFFramework/Android/ZFFramework_test/Loader.java
@@ -1,12 +1,24 @@
 package com.ZFFramework.Android.ZFFramework_test;
 
-import com.ZFFramework.Android.ZF_impl.ZFLoader;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
 
-public class Loader extends ZFLoader {
+import com.ZFFramework.Android.ZF_impl.ZFMainEntry;
 
+public class Loader extends Activity {
     static {
         System.loadLibrary("ZFFramework_test");
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            ZFMainEntry.debugMode(true);
+        }
+        ZFMainEntry.mainEntryFromLoader(this);
+    }
 }
 
--- tools/zfproj_creator/private/app/{ZFTT_R_proj_name}/zfproj_with_src/Android/{ZFTT_R_proj_name}/zfapp/src/main/java/com/ZFFramework/Android/{ZFTT_R_proj_name}/Loader.java
+++ tools/zfproj_creator/private/app/{ZFTT_R_proj_name}/zfproj_with_src/Android/{ZFTT_R_proj_name}/zfapp/src/main/java/com/ZFFramework/Android/{ZFTT_R_proj_name}/Loader.java
@@ -1,12 +1,24 @@
 package com.ZFFramework.Android.{ZFTT_R_proj_name};
 
-import com.ZFFramework.Android.ZF_impl.ZFLoader;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
 
-public class Loader extends ZFLoader {
+import com.ZFFramework.Android.ZF_impl.ZFMainEntry;
 
+public class Loader extends Activity {
     static {
         System.loadLibrary("{ZFTT_R_proj_name}");
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            ZFMainEntry.debugMode(true);
+        }
+        ZFMainEntry.mainEntryFromLoader(this);
+    }
 }
 
