f7de75c5b91ef9c04819ff4000ffe4e3843ba28c
ZSaberLv0 <z@zsaber.com>
2025-10-03 19:36:25 +0800

    update

---
+++ ZF/ZFUIKit_impl/zfsrc/ZFImpl/sys_SDL/ZFImpl_sys_SDL_AppIcon.cpp
@@ -0,0 +1,26 @@
+#include "ZFImpl_sys_SDL_Image.h"
+
+#if ZF_ENV_sys_SDL
+
+ZF_NAMESPACE_GLOBAL_BEGIN
+
+ZF_GLOBAL_INITIALIZER_INIT_WITH_LEVEL(ZFImpl_sys_SDL_AppIcon, ZFLevelZFFrameworkEssential) {
+    ZFImpl_sys_SDL_WindowOnCreate.add(zfself::_action);
+}
+ZF_GLOBAL_INITIALIZER_DESTROY(ZFImpl_sys_SDL_AppIcon) {
+    ZFImpl_sys_SDL_WindowOnCreate.removeElement(zfself::_action);
+}
+public:
+    static void _action(ZF_IN SDL_Window *owner) {
+        ZFInput input = ZFInputForRes("AppIcon.png");
+        if(input) {
+            SDL_Surface *sdlSurface = IMG_Load_IO(ZFImpl_sys_SDL_ZFInputToSDL_IOStream(input), true);
+            SDL_SetWindowIcon(owner, sdlSurface);
+            SDL_DestroySurface(sdlSurface);
+        }
+    }
+ZF_GLOBAL_INITIALIZER_END(ZFImpl_sys_SDL_AppIcon)
+
+ZF_NAMESPACE_GLOBAL_END
+#endif // #if ZF_ENV_sys_SDL
+
