3a46f1b3e40f505e0dabcd308e6d0be238ef849c
ZSaberLv0 <z@zsaber.com>
2025-10-07 00:27:15 +0800

    update

--- demo/zfproj/cmake/ZFFramework_test/cmake_main.cpp
+++ demo/zfproj/cmake/ZFFramework_test/cmake_main.cpp
@@ -7,9 +7,34 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
 
+#if ZF_ENV_sys_Windows
+#include <Windows.h>
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    LoadLibraryA("ZFCore");
+    LoadLibraryA("ZFAlgorithm");
+    LoadLibraryA("ZFUtility");
+    LoadLibraryA("ZFUIKit");
+    LoadLibraryA("ZFUIWidget");
+    LoadLibraryA("ZFUIExt");
+    LoadLibraryA("ZFLua");
+    LoadLibraryA("ZFUIWebKit");
+    LoadLibraryA("ZFNet");
+    LoadLibraryA("ZFAppUtil");
+    LoadLibraryA("ZF_impl");
+    LoadLibraryA("ZFCore_impl");
+    LoadLibraryA("ZFAlgorithm_impl");
+    LoadLibraryA("ZFUIKit_impl");
+    LoadLibraryA("ZFLua_impl");
+    LoadLibraryA("ZFUIWebKit_impl");
+    LoadLibraryA("ZFNet_impl");
+
+    return ZFImpl_sys_SDL_AppInit(appstate, argc, argv);
+}
+#else
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     return ZFImpl_sys_SDL_AppInit(appstate, argc, argv);
 }
+#endif
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     return ZFImpl_sys_SDL_AppEvent(appstate, event);
 }
