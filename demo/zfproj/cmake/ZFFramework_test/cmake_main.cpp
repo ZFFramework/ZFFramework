770830fd8a78d01630c13e14c58fcd5443a370ef
ZSaberLv0 <z@zsaber.com>
2025-10-07 22:17:10 +0800

    update

--- demo/zfproj/cmake/ZFFramework_test/cmake_main.cpp
+++ demo/zfproj/cmake/ZFFramework_test/cmake_main.cpp
@@ -1,15 +1,13 @@
+#define SDL_MAIN_HANDLED
+
+#include "ZFCore.h"
+#include "ZFCore/protocol/ZFProtocolZFMainEntry.h"
+
 #include "ZFImpl/ZFImpl_env.h"
-
-#if ZF_ENV_sys_SDL
-#include "ZFImpl/sys_SDL/ZFMainEntry_sys_SDL.h"
-
-#define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL_init.h>
 
 #if ZF_ENV_sys_Windows
 #include <Windows.h>
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     LoadLibraryA("ZFCore");
     LoadLibraryA("ZFAlgorithm");
     LoadLibraryA("ZFUtility");
@@ -28,22 +26,27 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     LoadLibraryA("ZFUIWebKit_impl");
     LoadLibraryA("ZFNet_impl");
 
-    return ZFImpl_sys_SDL_AppInit(appstate, argc, argv);
-}
-#else
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-    return ZFImpl_sys_SDL_AppInit(appstate, argc, argv);
-}
-#endif
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-    return ZFImpl_sys_SDL_AppEvent(appstate, event);
-}
-SDL_AppResult SDL_AppIterate(void *appstate) {
-    return ZFImpl_sys_SDL_AppIterate(appstate);
-}
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    return ZFImpl_sys_SDL_AppQuit(appstate, result);
-}
+    ZFFrameworkInit();
 
-#endif
+    ZFCoreArray<zfstring> params;
+    ZFCoreArgSplit(params, lpCmdLine);
+    zfint ret = ZFMainExecute(params);
+
+    ZFFrameworkCleanup();
+    return (int)ret;
+}
+#else // #if ZF_ENV_sys_Windows
+int main(int argc, char **argv) {
+    ZFFrameworkInit();
+
+    ZFCoreArray<zfstring> params;
+    for(int i = 1; i < argc; ++i) {
+        params.add(argv[i]);
+    }
+    zfint ret = ZFMainExecute(params);
+
+    ZFFrameworkCleanup();
+    return (int)ret;
+}
+#endif // #if ZF_ENV_sys_Windows
 
