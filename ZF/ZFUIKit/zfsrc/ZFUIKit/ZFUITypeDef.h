c0bbdf45709d823cf8c1b7c0973b645cf9621047
ZSaberLv0 <z@zsaber.com>
2026-03-08 16:11:58 +0800

    update

--- ZF/ZFUIKit/zfsrc/ZFUIKit/ZFUITypeDef.h
+++ ZF/ZFUIKit/zfsrc/ZFUIKit/ZFUITypeDef.h
@@ -703,16 +703,11 @@ ZFMETHOD_FUNC_INLINE_DECLARE_2(ZFLIB_ZFUIKit, ZFUIRect, ZFUIRectUnion
         ) {
     if(ZFUIRectIsEmpty(rect1)) {return rect2;}
     if(ZFUIRectIsEmpty(rect2)) {return rect1;}
-    ZFUIRect ret = {
-        (rect1.x < rect2.x) ? rect1.x : rect2.x,
-        (rect1.y < rect2.y) ? rect1.y : rect2.y,
-        (rect1.x + rect1.width > rect2.x + rect2.width)
-            ? (rect1.x + rect1.width)
-            : (rect2.x + rect2.width),
-        (rect1.y + rect1.height > rect2.y + rect2.height)
-            ? (rect1.y + rect1.height)
-            : (rect2.y + rect2.height)
-    };
+    ZFUIRect ret;
+    ret.x = (rect1.x < rect2.x) ? rect1.x : rect2.x;
+    ret.y = (rect1.y < rect2.y) ? rect1.y : rect2.y;
+    ret.width = (rect1.x + rect1.width > rect2.x + rect2.width) ? (rect1.x + rect1.width - ret.x) : (rect2.x + rect2.width - ret.x);
+    ret.height = (rect1.y + rect1.height > rect2.y + rect2.height) ? (rect1.y + rect1.height - ret.y) : (rect2.y + rect2.height - ret.y);
     if(ZFUIRectIsEmpty(ret)) {
         return ZFUIRectZero();
     }
@@ -729,16 +724,11 @@ ZFMETHOD_FUNC_INLINE_DECLARE_2(ZFLIB_ZFUIKit, ZFUIRect, ZFUIRectIntersection
         ) {
     if(ZFUIRectIsEmpty(rect1)) {return rect2;}
     if(ZFUIRectIsEmpty(rect2)) {return rect1;}
-    ZFUIRect ret = {
-        (rect1.x > rect2.x) ? rect1.x : rect2.x,
-        (rect1.y > rect2.y) ? rect1.y : rect2.y,
-        (rect1.x + rect1.width < rect2.x + rect2.width)
-            ? (rect1.x + rect1.width)
-            : (rect2.x + rect2.width),
-        (rect1.y + rect1.height < rect2.y + rect2.height)
-            ? (rect1.y + rect1.height)
-            : (rect2.y + rect2.height)
-    };
+    ZFUIRect ret;
+    ret.x = (rect1.x > rect2.x) ? rect1.x : rect2.x;
+    ret.y = (rect1.y > rect2.y) ? rect1.y : rect2.y;
+    ret.width = (rect1.x + rect1.width < rect2.x + rect2.width) ? (rect1.x + rect1.width - ret.x) : (rect2.x + rect2.width - ret.x);
+    ret.height = (rect1.y + rect1.height < rect2.y + rect2.height) ? (rect1.y + rect1.height - ret.y) : (rect2.y + rect2.height - ret.y);
     if(ZFUIRectIsEmpty(ret)) {
         return ZFUIRectZero();
     }
